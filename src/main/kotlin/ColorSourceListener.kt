408e95d37222faf48aee501b7dc23368e0055a1c
verity <joshua-schmidt@online.de>
2022-12-03 14:02:15 +0100

    :boom: Introduce ColorSourceListener

---
+++ src/main/kotlin/ColorSourceListener.kt
@@ -0,0 +1,53 @@
+import ch.bildspur.artnet.ArtNetBuffer
+import ch.bildspur.artnet.ArtNetClient
+import ch.bildspur.artnet.events.ArtNetServerEventAdapter
+import ch.bildspur.artnet.packets.ArtNetPacket
+
+object ColorSourceListener {
+
+    private val artNetBuffer = ArtNetBuffer()
+    private val artNetClient = ArtNetClient(artNetBuffer)
+
+    fun startListening(universes: IntArray) {
+        artNetClient.artNetServer.addListener(object : ArtNetServerEventAdapter() {
+            override fun artNetPacketReceived(packet: ArtNetPacket?) {
+                if (packet !== null) {
+                    for (universe in universes) {
+                        handleIncomingPacketsOfUniverse(packet, universe)
+                    }
+                } else {
+                    println("[WARNING]: Received packet is null")
+                }
+            }
+        })
+
+        artNetClient.start()
+    }
+
+    /**
+     * Method to handle incoming packets for specified universe
+     * @param packet The incoming [ArtNetPacket]
+     * @param universe The universe to handle
+     */
+    private fun handleIncomingPacketsOfUniverse(packet: ArtNetPacket, universe: Int) {
+        println("[UNIVERSE $universe]: New packet received - ${packet.data}")
+
+        val encodedData: ByteArray = artNetClient.readDmxData(0, universe)
+        val decodedData: MutableList<Int> = mutableListOf()
+
+        for (address in 0..511) {
+            decodedData.add(encodedData[address].toInt() and 0xFF)
+        }
+
+        handleEncodedData(decodedData.toIntArray(), universe)
+    }
+
+    /**
+     * Handles encoded data
+     * @param data Encoded data as [IntArray]
+     */
+    private fun handleEncodedData(data: IntArray, universes: Int) {
+        println("[UNIVERSE $universes]: Decoded - ${data.joinToString()}")
+    }
+
+}
