cb8824b125306393200cf67adc049f0786538a22
verity <joshua-schmidt@online.de>
2022-12-03 20:06:36 +0100

    :memo: Add kDocs to Logger.kt

--- src/main/kotlin/utils/Logger.kt
+++ src/main/kotlin/utils/Logger.kt
@@ -5,15 +5,28 @@ import java.util.*
 
 object Logger {
 
+    /**
+     * Whether debug logs should be displayed or not
+     */
     private var enableDebug = true
+
+    /**
+     * Whether art-net logs should be displayed or not
+     */
     private var enableArtNet = true
+
+    /**
+     * Whether logic logs should be displayed or not
+     */
     private var enableLogic = true
 
-    private fun getFormattedTime() =  SimpleDateFormat("HH:mm:ss").format(Date())
+    /**
+     * Get current time in correct date format (HH:mm:ss)
+     */
+    private fun getFormattedTime() = SimpleDateFormat("HH:mm:ss").format(Date())
 
     private const val reset = "\u001B[0m"
     private const val grey = "\u001B[90m"
-
     private const val cyan = "\u001B[36m"
     private const val red = "\u001B[31m"
     private const val orange = "\u001B[33m"
@@ -21,38 +34,66 @@ object Logger {
     private const val purple = "\u001B[35m"
     private const val yellow = "\u001B[33m"
 
+
+    /**
+     * Sets logger settings from configuration
+     */
     fun configure() {
         enableDebug = Configuration.config.enableDebugLogs
         enableArtNet = Configuration.config.enableArtNetLogs
         enableLogic = Configuration.config.enableLogicLogs
     }
 
+    /**
+     * Log a text as info log
+     * @param msg The logging text
+     */
     fun info(msg: String) {
         println("$grey[${getFormattedTime()}]: [${green}INFO$grey] $reset$msg")
     }
 
+    /**
+     * Log a text as warn log
+     * @param msg The logging text
+     */
     fun warn(msg: String) {
         println("$grey[${getFormattedTime()}]: [${orange}WARNING$grey] $reset$msg")
     }
 
+    /**
+     * Log a text as error log
+     * @param msg The logging text
+     */
     fun error(msg: String) {
         println("$grey[${getFormattedTime()}]: [${red}ERROR$grey] $red$msg")
     }
 
+    /**
+     * Log a text as debug log
+     * @param msg The logging text
+     */
     fun debug(msg: String) {
-        if(enableDebug) {
+        if (enableDebug) {
             println("$grey[${getFormattedTime()}]: [${cyan}DEBUG$grey] $reset$msg")
         }
     }
 
+    /**
+     * Log a text as art-net log
+     * @param msg The logging text
+     */
     fun artNet(msg: String) {
-        if(enableArtNet) {
+        if (enableArtNet) {
             println("$grey[${getFormattedTime()}]: [${purple}ART-NET$grey] $reset$msg")
         }
     }
 
+    /**
+     * Log a text as logic log
+     * @param msg The logging text
+     */
     fun logic(msg: String) {
-        if(enableLogic) {
+        if (enableLogic) {
             println("$grey[${getFormattedTime()}]: [${yellow}LOGIC$grey] $reset$msg")
         }
     }
