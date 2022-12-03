5238cde75dbd4cdf50fdb4627df2654f6e7b00fb
verity <joshua-schmidt@online.de>
2022-12-03 20:03:35 +0100

    :memo: Add kDocs to Configuration.kt

--- src/main/kotlin/utils/Configuration.kt
+++ src/main/kotlin/utils/Configuration.kt
@@ -7,11 +7,24 @@ import java.io.File
 
 object Configuration {
 
+    /**
+     * Variable to access configuration values
+     */
     lateinit var config: Config
 
+    /**
+     * Configuration [File]
+     */
     private val file = File("config.json")
+
+    /**
+     * Gson Instance
+     */
     private val gson = GsonBuilder().setPrettyPrinting().create()
 
+    /**
+     * Method to initialize the configuration - create file (if necessary) and read values
+     */
     fun initialize() {
         Logger.debug("Initializing configuration...")
 
@@ -23,6 +36,9 @@ object Configuration {
         readConfig()
     }
 
+    /**
+     * Method to create the default configuration file
+     */
     private fun createFileFromDefault() {
         Logger.warn("Creating config file from default...")
 
@@ -33,6 +49,9 @@ object Configuration {
         file.writeText(jsonString)
     }
 
+    /**
+     * Method to read configuration file - save in [config]
+     */
     private fun readConfig() {
         Logger.debug("Reading configuration...")
 
